f95c982d2ec1954dc9e48cfcdf77f16c423c0cf0
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-08-26 12:10:10 +0300

    Uc допил

--- UserControlsLib/InputTextFieldUc.xaml.cs
+++ UserControlsLib/InputTextFieldUc.xaml.cs
@@ -8,6 +8,23 @@ public partial class InputTextFieldUc : UserControl
     public InputTextFieldUc() {
         InitializeComponent();
     }
+    
+    public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register(
+         nameof(InputText),
+         typeof(string),
+         typeof(InputTextFieldUc),
+         new FrameworkPropertyMetadata(string.Empty, OnInputTextChanged)
+         );
+
+    public string InputText {
+        get => (string)GetValue(InputTextProperty);
+        set => SetValue(InputTextProperty, value);
+    }
+
+    private static void OnInputTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        
+    }
+    
     private void ClearButton_OnClick(object sender, RoutedEventArgs e) {
         InputTextBox.Clear();
         InputTextBox.Focus();
