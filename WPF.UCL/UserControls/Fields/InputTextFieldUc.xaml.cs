e53be03acef2bd0b8ffe3e04d721935c65cc62a1
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-08-26 17:03:11 +0300

    развекаюсь

--- WPF.UCL/UserControls/Fields/InputTextFieldUc.xaml.cs
+++ WPF.UCL/UserControls/Fields/InputTextFieldUc.xaml.cs
@@ -1,7 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WPF.UCL.UserControls;
+namespace WPF.UCL.UserControls.Fields;
 
 public partial class InputTextFieldUc : UserControl
 {
