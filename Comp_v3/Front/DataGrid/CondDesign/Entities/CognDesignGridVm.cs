93cf4091b69b3273135060d37a4fb1e3b02c2d91
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-09-01 16:36:38 +0300

    стало чуть легче дышать

--- Comp_v3/Front/DataGrid/CondDesign/Entities/CognDesignGridVm.cs
+++ Comp_v3/Front/DataGrid/CondDesign/Entities/CognDesignGridVm.cs
@@ -7,35 +7,23 @@ using Comp_v3.Front.Events;
 using Comp.Db.Contracts;
 using Comp.ModelData.TechnicalItems;
 using Component_v2.Tools.EventBus;
+using Utils.WPF.VmEnumerableInteractiveData;
 
 namespace Comp_v3.Front.DataGrid.CondDesign.Entities;
 
-public class CognDesignGridVm : ObservableObject, ICellEditEndingHandler, ICellAddingToDataGridHandler, ICancelNewItemAddingHandler
+public class CognDesignGridVm : VmEnumerableInteractiveData<ConditionalDesignation>, ICellEditEndingHandler, ICellAddingToDataGridHandler, ICancelNewItemAddingHandler
 {
-    private ConditionalDesignation? _selectedItem; /* база 100% */
-
     public CognDesignGridVm(IConditionalDesignationRepository repository, StateProviderDg stateProviderDg) { /* Ui-взаимодействующий : VmRepo */
         Repository = repository;
         StateProvider = stateProviderDg;
         EventBus<IUiGlobalSubscriber>.Subscribe(this);
         LoadDataAsync();
     }
-
     public virtual void Dispose() => EventBus<IUiGlobalSubscriber>.Unsubscribe(this);  /* Ui-взаимодействующий : VmRepo */
 
     public IConditionalDesignationRepository Repository { get; } /* VmRepo : базы */
 
     public StateProviderDg StateProvider { get; } /* Ui-взаимодействующий : VmRepo */
-
-    public required ObservableCollection<ConditionalDesignation?> Items { get; set; } /* свойство должно быть обязательно инициализировано при создании объекта */ /* база 100% */
-
-    public ConditionalDesignation? SelectedItem { /* база 100% */
-        get => _selectedItem;
-        set {
-            _selectedItem = value;
-            OnPropertyChanged();
-        }
-    }
 
     private async void LoadDataAsync() {  /* VmRepo : базы */
         var items = await Repository.GetAllAsync();
--- Utils/WPF/VmEnumerableInteractiveData/VmEnumerableInteractiveData.cs
+++ Utils/WPF/VmEnumerableInteractiveData/VmEnumerableInteractiveData.cs
@@ -1,6 +1,18 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Utils.WPF.VmEnumerableInteractiveData;
 
-public class VmEnumerableInteractiveData
+public class VmEnumerableInteractiveData<T> : ObservableObject
 {
-    
+    public required ObservableCollection<T?> Items { get; set; } /* свойство должно быть обязательно инициализировано при создании объекта */
+
+    private T? _selectedItem;
+    public T? SelectedItem { /* база 100% */
+        get => _selectedItem;
+        set {
+            _selectedItem = value;
+            OnPropertyChanged();
+        }
+    }
 }
