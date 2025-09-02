5a93a09ed72522b6bb6620f69bf3fe9a41dd1487
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-09-02 17:19:06 +0300

    Воот, как то так

--- Infrastructure/Command/TransactionSupportive/AdvancedUndoRedoManager.cs
+++ Infrastructure/Command/TransactionSupportive/AdvancedUndoRedoManager.cs
@@ -7,15 +7,12 @@ public interface ICommandSheduler<in T> where T : ICommand
     Task RedoAsync();
 }
 
-public class HeterochCommandSheduler : AdvancedUndoRedoManager
+public class HeterochCommandSheduler<TCommand> : AdvancedUndoRedoManager<TCommand> 
+    where TCommand : ICommand, IDeferredCommand
 {
-    /*public async Task ExecuteCommand(IDeferredCommand command) {
-        base.ExecuteCommand(command);
+    public override async Task ExecuteCommand(TCommand command) {
+        await base.ExecuteCommand(command);
         await command.ExecuteDeferredAsync();
-    }*/
-
-    public override async Task ExecuteCommand(IDeferredCommand command) {
-        return base.ExecuteCommand(command);
     }
 
     public Task UndoAsync() {
@@ -28,30 +25,31 @@ public class HeterochCommandSheduler : AdvancedUndoRedoManager
 }
 
 /* Redo для транзакций не реализован */
-public class AdvancedUndoRedoManager<T> : ICommandSheduler<ICommand>
+public class AdvancedUndoRedoManager<TCommand> : ICommandSheduler<TCommand> 
+    where TCommand : ICommand
 {
-    private readonly Stack<ICommand> _undoStack = new();
-    private readonly Stack<ICommand> _redoStack = new();
-    private CompositeCommandBase _currentTransaction;
+    private readonly Stack<TCommand> _undoStack = new();
+    private readonly Stack<TCommand> _redoStack = new();
+    //private CompositeCommandBase _currentTransaction;
 
     public bool CanUndo => _undoStack.Count > 0;
     public bool CanRedo => _redoStack.Count > 0;
     
-    public bool IsInTransaction => _currentTransaction != null;
+    //public bool IsInTransaction => _currentTransaction != null;
 
     public event EventHandler<CommandExecutedEventArgs> CommandExecuted;
 
-    public virtual async Task ExecuteCommand(ICommand command) {
-        if (_currentTransaction != null) {
+    public virtual async Task ExecuteCommand(TCommand command) {
+        /*if (_currentTransaction != null) {
             _currentTransaction.AddCommand(command);
             await command.ExecuteAsync().ConfigureAwait(false);
         }
-        else {
+        else {*/
             await command.ExecuteAsync().ConfigureAwait(false);
             _undoStack.Push(command);
             _redoStack.Clear();
             CommandExecuted?.Invoke(this, new CommandExecutedEventArgs(command, CommandAction.Executed));
-        }
+        /*}*/
     }
 
     public virtual async Task UndoAsync() {
@@ -72,7 +70,7 @@ public class AdvancedUndoRedoManager<T> : ICommandSheduler<ICommand>
         CommandExecuted?.Invoke(this, new CommandExecutedEventArgs(command, CommandAction.Redone));
     }
 
-    public void BeginTransaction(string transactionName = null) {
+    /*public void BeginTransaction(string transactionName = null) {
         if (_currentTransaction != null)
             throw new InvalidOperationException("Transaction already started");
         
@@ -107,5 +105,5 @@ public class AdvancedUndoRedoManager<T> : ICommandSheduler<ICommand>
     {
         private readonly string _name;
         public TransactionCommand(string name) => _name = name;
-    }
+    }*/
 }
