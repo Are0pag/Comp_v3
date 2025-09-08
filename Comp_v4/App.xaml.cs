a167204ce1759e6d8fcad787a7a5b697274cb015
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-09-08 15:22:32 +0300

    Ссылки и стартап

--- Comp_v4/App.xaml.cs
+++ Comp_v4/App.xaml.cs
@@ -1,14 +1,52 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Comp.Db;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Comp_v4
+namespace Comp_v4;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
+    public static IHost Host { get; protected set; }
+    protected IServiceScope _mainScope;
+
+    public App() {
+        Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder().
+                         ConfigureServices((hostContext, services) => {
+                           
+
+                         }).Build();
+    }
+    
+    /* Также можно явно указать зависимость:
+     services.AddSingleton<DataGridManageButtonsVm>(provider =>
+        new DataGridManageButtonsVm(
+            provider.GetService<IConditionalDesignationRepository>(),
+            provider.GetService<CognDesignGridVm>()
+    )
+     */
+    
+    protected override async void OnStartup(StartupEventArgs e) {
+        await Host.StartAsync();
+        
+        // Создаем БД при старте (используем scope)
+        using (var scope = Host.Services.CreateScope()) {
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await context.Database.EnsureCreatedAsync();
+        } 
+        
+        /*_mainScope = Host.Services.CreateScope();
+        var mainWindow = _mainScope.ServiceProvider.GetRequiredService<CognDesignGridWindow>(); 
+        mainWindow.Closed += (_, _) => _mainScope?.Dispose();
+        mainWindow.Show();*/
+        
+        base.OnStartup(e);
     }
 
+    protected override async void OnExit(ExitEventArgs e) {
+        _mainScope?.Dispose();
+        await Host.StopAsync();
+        Host.Dispose();
+        base.OnExit(e);
+    }
 }
