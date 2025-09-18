9bf15999710323c1636c9160222b4aeda525ddc4
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-09-18 14:58:51 +0300

    Оно запускается, ахахахаха

--- Comp_v4/Installers/App.xaml.cs
+++ Comp_v4/Installers/App.xaml.cs
@@ -1,17 +1,23 @@
 ﻿using System.Windows;
+using Castle.Windsor;
 using Comp_v4.Entities;
 using Comp_v4.Operations.Commands;
+using Comp_v4.Operations.Commands.Filtering;
 using Comp.Db;
 using Comp.Db.Contracts;
-using Comp.ModelData.TechnicalItems;
+using Infrastructure.Command;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WPF.Services.UserActionsHandling.InputText;
+using WPF.Services.Validation;
 using WPF.Templates;
 using WPF.Templates.TableWindow.States;
 using WPF.Templates.TableWindow.Vm;
 using WPF.Templates.TableWindow.Vm.Components;
+
+using Tw = Comp_v4.TargetWindow;
+using Cd = Comp.ModelData.TechnicalItems.ConditionalDesignation;
 
 namespace Comp_v4;
 
@@ -23,52 +29,58 @@ public partial class App : Application
     protected List<IDisposable> _disposable;
     
     public App() {
+        
+        
+        
+        
+        
+        
         Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder().
                          ConfigureServices((hostContext, s /* services */) => {
                              
                              s.AddDbContext<AppDbContext>(options => {
                                  options.UseSqlite(DbConfig.ConnectionString);
                              });
-                             s.AddTransient<IRepository<ConditionalDesignation>, ConditionalDesignationRepository>();
+                             s.AddTransient<IRepository<Cd>, ConditionalDesignationRepository>();
 
-                             System.Windows.Threading.Dispatcher disp = App.Current.Dispatcher;
-                             var d = App.Current.Dispatcher;
+                             s.AddSingleton<System.Windows.Threading.Dispatcher>(provider => App.Current.Dispatcher);
                              
-                             s.AddSingleton<CommandFactory>(provider => new CommandFactory(provider));
-                             s.AddSingleton<Validator>();
+                             s.AddSingleton<ICommandFactory, CommandFactory>(provider => new CommandFactory(provider));
+                             s.AddSingleton<ValidatorBase<Cd>, Validator>();
 
                              s.AddSingleton<IDataGridCommandScheduler, DataGridCommandScheduler>();
-                             s.AddTransient<DataGridPropertyRestoreService<ConditionalDesignation>>();
+                             s.AddTransient<DataGridPropertyRestoreService<Cd>>();
+                             s.AddSingleton<IFilter<Cd, FiltersVmCd>, Filter>();
 
-                             s.AddSingleton<DataGridViewModel<ConditionalDesignation>>();
+                             s.AddSingleton<DataGridViewModel<Cd>>();
                              s.AddSingleton<FiltersVmCd>();
 
-                             s.AddSingleton<ModuleContext<TargetWindow, ConditionalDesignation>>();
+                             s.AddSingleton<ModuleContext<Tw, Cd>>();
 
-                             s.AddScoped<ActionStartAddingNewItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddScoped<ActionAddItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddScoped<ActionUpdateItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddScoped<ActionDeleteItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddScoped<ActionSave<TargetWindow, ConditionalDesignation>>();
+                             s.AddScoped<ActionStartAddingNewItem<Tw, Cd>>();
+                             s.AddScoped<ActionAddItem<Tw, Cd>>();
+                             s.AddScoped<ActionUpdateItem<Tw, Cd>>();
+                             s.AddScoped<ActionDeleteItem<Tw, Cd>>();
+                             s.AddScoped<ActionSave<Tw, Cd>>();
                              
-                             s.AddSingleton<CellStateIdle<TargetWindow, ConditionalDesignation>>();
-                             s.AddSingleton<CellStateUpdate<TargetWindow, ConditionalDesignation>>();
-                             s.AddSingleton<CellStateAddItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddSingleton<BaseCellState<TargetWindow, ConditionalDesignation>>(provider => provider.GetRequiredService<CellStateIdle<TargetWindow, ConditionalDesignation>>());
-                             s.AddSingleton<BaseCellState<TargetWindow, ConditionalDesignation>>(provider => provider.GetRequiredService<CellStateUpdate<TargetWindow, ConditionalDesignation>>());
-                             s.AddSingleton<BaseCellState<TargetWindow, ConditionalDesignation>>(provider => provider.GetRequiredService<CellStateAddItem<TargetWindow, ConditionalDesignation>>());
-                             s.AddSingleton<Cell<TargetWindow, ConditionalDesignation>>(provider => {
-                                                   var states = provider.GetServices<BaseCellState<TargetWindow, ConditionalDesignation>>();
-                                                   var initialState = provider.GetService<CellStateIdle<TargetWindow, ConditionalDesignation>>();
-                                                   return new Cell<TargetWindow, ConditionalDesignation>(states, initialState!);
+                             s.AddSingleton<CellStateIdle<Tw, Cd>>();
+                             s.AddSingleton<CellStateUpdate<Tw, Cd>>();
+                             s.AddSingleton<CellStateAddItem<Tw, Cd>>();
+                             s.AddSingleton<BaseCellState<Tw, Cd>>(provider => provider.GetRequiredService<CellStateIdle<Tw, Cd>>());
+                             s.AddSingleton<BaseCellState<Tw, Cd>>(provider => provider.GetRequiredService<CellStateUpdate<Tw, Cd>>());
+                             s.AddSingleton<BaseCellState<Tw, Cd>>(provider => provider.GetRequiredService<CellStateAddItem<Tw, Cd>>());
+                             s.AddSingleton<Cell<Tw, Cd>>(provider => {
+                                                   var states = provider.GetServices<BaseCellState<Tw, Cd>>();
+                                                   var initialState = provider.GetService<CellStateIdle<Tw, Cd>>();
+                                                   return new Cell<Tw, Cd>(states, initialState!);
                                                });
                              
 
-                             s.AddScoped<ButtonVmAddItem<TargetWindow, ConditionalDesignation>>();
-                             s.AddSingleton<ButtonVmSave<TargetWindow, ConditionalDesignation>>();
-                             s.AddSingleton<ButtonVmDeleteItem<TargetWindow, ConditionalDesignation>>();
+                             s.AddScoped<ButtonVmAddItem<Tw, Cd>>();
+                             s.AddSingleton<ButtonVmSave<Tw, Cd>>();
+                             s.AddSingleton<ButtonVmDeleteItem<Tw, Cd>>();
                              
-                             s.AddTransient<TargetWindow>();
+                             s.AddTransient<Tw>();
 
                          }).Build();
     }
@@ -84,7 +96,7 @@ public partial class App : Application
         _mainScope = Host.Services.CreateScope();
         CreateRequiredInstancesManually();
 
-        var mainWindow = _mainScope.ServiceProvider.GetRequiredService<TargetWindow>(); 
+        var mainWindow = _mainScope.ServiceProvider.GetRequiredService<Tw>(); 
         mainWindow.Closed += (_, _) => {
             foreach (var d in _disposable) d.Dispose();
             _disposable.Clear();
@@ -108,18 +120,18 @@ public partial class App : Application
     protected virtual void CreateRequiredInstancesManually() {
         var scheduler = _mainScope.ServiceProvider.GetRequiredService<IDataGridCommandScheduler>();
         var filtersVm = _mainScope.ServiceProvider.GetRequiredService<FiltersVmCd>();
-        var mContext = _mainScope.ServiceProvider.GetRequiredService<ModuleContext<TargetWindow, ConditionalDesignation>>();
-        var cell = _mainScope.ServiceProvider.GetRequiredService<Cell<TargetWindow, ConditionalDesignation>>();
-        var comFactory = _mainScope.ServiceProvider.GetRequiredService<CommandFactory>();
+        var mContext = _mainScope.ServiceProvider.GetRequiredService<ModuleContext<Tw, Cd>>();
+        var cell = _mainScope.ServiceProvider.GetRequiredService<Cell<Tw, Cd>>();
+        var comFactory = _mainScope.ServiceProvider.GetRequiredService<ICommandFactory>();
         
         _disposable = new List<IDisposable>() {
             new ActionStackTracker(scheduler),
-            new PersistenceManager<TargetWindow, ConditionalDesignation>(scheduler, _mainScope.ServiceProvider.GetRequiredService<ActionSave<TargetWindow, ConditionalDesignation>>()),
-            new TableCommandBinderFilteringCompatible<TargetWindow, ConditionalDesignation>(
-                                                      _mainScope.ServiceProvider.GetRequiredService<ActionStartAddingNewItem<TargetWindow, ConditionalDesignation>>(), 
-                                                      _mainScope.ServiceProvider.GetRequiredService<ActionDeleteItem<TargetWindow, ConditionalDesignation>>()
-                                                      ),
-            new ActionFilter<TargetWindow, ConditionalDesignation, FiltersVmCd>(scheduler, mContext, comFactory, filtersVm, cell)
+            new PersistenceManager<Tw, Cd>(scheduler, _mainScope.ServiceProvider.GetRequiredService<ActionSave<Tw, Cd>>()),
+            new TableCommandBinderFilteringCompatible<Tw, Cd>(
+                                                              _mainScope.ServiceProvider.GetRequiredService<ActionStartAddingNewItem<Tw, Cd>>(), 
+                                                              _mainScope.ServiceProvider.GetRequiredService<ActionDeleteItem<Tw, Cd>>()
+                                                             ),
+            new ActionFilter<Tw, Cd, FiltersVmCd>(scheduler, mContext, comFactory, filtersVm, cell)
         };
     }
 }
---
+++ Comp_v4/TableWindows/ConditionalDesignation/Installer.cs
@@ -0,0 +1,12 @@
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Comp_v4.TableWindows.ConditionalDesignation;
+
+public class Installer : IWindsorInstaller
+{
+    public void Install(IWindsorContainer container, IConfigurationStore store) {
+        throw new NotImplementedException();
+    }
+}
