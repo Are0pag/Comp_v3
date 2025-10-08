8d60c2be9f8367e25fa8d53a5968d7dc6a58e469
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-08 12:26:15 +0300

    Так должно добавляться в БД

--- Comp_v4/CompCard/Entities/States/Base/BaseStateCardComp.cs
+++ Comp_v4/CompCard/Entities/States/Base/BaseStateCardComp.cs
@@ -1,18 +1,22 @@
 using Comp_v4.CompCard.Entities.Validation;
+using Comp_v4.CompCard.Events;
 using Comp.Db.Contracts;
 using Comp.ModelData.Comp;
 using Comp.ModelData.SortingItems;
+using Comp.ModelData.TechnicalItems;
 using Infrastructure.StateMachine;
+using Utils.EventBus;
 
 namespace Comp_v4.CompCard.Entities.States;
 
-public abstract class BaseStateCardComp : StateBase<CardComp>
+public abstract class BaseStateCardComp : StateBase<CardComp>, IExternalTableInputHandler 
 {
     protected readonly Component _component;
     protected readonly IRepository<Component> _repository;
     protected readonly CardCopmEditController _editController;
 
     protected BaseStateCardComp(Component component, IRepository<Component> repository, CardCopmEditController editController) {
+        EventBus<ICompCardSubscriber>.Subscribe(this);
         _component = component;
         _repository = repository;
         _editController = editController;
@@ -20,5 +24,16 @@ public abstract class BaseStateCardComp : StateBase<CardComp>
 
     public virtual void Save(CardComp card) {
         
+    }
+
+    public void Dispose() {
+        EventBus<ICompCardSubscriber>.Unsubscribe(this);
+    }
+
+    public virtual void HandleTableInput(object? args) {
+        if (args is not ConditionalDesignation cd) 
+            return;
+
+        _component.ConditionalDesignation = cd;
     }
 }
