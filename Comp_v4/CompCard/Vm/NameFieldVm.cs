448ea25c540efadcbd041dc7af73c274638d2f23
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-09 11:10:58 +0300

    GP поля загружаются вместе с карточкой компонента

--- Comp_v4/CompCard/Vm/NameFieldVm.cs
+++ Comp_v4/CompCard/Vm/NameFieldVm.cs
@@ -1,6 +1,5 @@
 using Comp_v4.CompCard.Entities.Validation;
-using WPF.Services.Validation;
-using WPF.Services.ValidationString;
+using Comp.ModelData.Comp;
 
 namespace Comp_v4.CompCard.Vm;
 
@@ -8,11 +7,19 @@ public class NameFieldVm : BaseRequiredTextFieldVm
 {
     public NameFieldVm(ValidatorName validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Name;
+    }
 }
 
 public class NomenclatureNumberFieldVm : BaseRequiredTextFieldVm
 {
     public NomenclatureNumberFieldVm(ValidatorNomNumber validator) : base(validator) {
+    }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.NomenclatureNumber;
     }
 }
 
@@ -20,11 +27,19 @@ public class CatalogNumberFieldVm : BaseTextFieldVm
 {
     public CatalogNumberFieldVm(ValidatorCatalogNumber validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.CatalogNumber;
+    }
 }
 
 public class LabelingOptionsFieldVm : BaseTextFieldVm
 {
     public LabelingOptionsFieldVm(ValidatorLabelingOptions validator) : base(validator) {
+    }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.LabelingOptions;
     }
 }
 
@@ -32,11 +47,19 @@ public class CodeOfElementFieldVm : BaseTextFieldVm
 {
     public CodeOfElementFieldVm(ValidatorCodeOfElement validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.CodeOfElement;
+    }
 }
 
 public class gpMainFieldVm : BaseTextFieldVm
 {
     public gpMainFieldVm(ValidatorGp validator) : base(validator) {
+    }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.GpMain;
     }
 }
 
@@ -44,11 +67,19 @@ public class gp1FieldVm : BaseTextFieldVm
 {
     public gp1FieldVm(ValidatorGp validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Gp1;
+    }
 }
 
 public class gp2FieldVm : BaseTextFieldVm
 {
     public gp2FieldVm(ValidatorGp validator) : base(validator) {
+    }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Gp2;
     }
 }
 
@@ -56,16 +87,28 @@ public class gp3FieldVm : BaseTextFieldVm
 {
     public gp3FieldVm(ValidatorGp validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Gp3;
+    }
 }
 
 public class gp4FieldVm : BaseTextFieldVm
 {
     public gp4FieldVm(ValidatorGp validator) : base(validator) {
     }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Gp4;
+    }
 }
 
 public class gp5FieldVm : BaseTextFieldVm
 {
     public gp5FieldVm(ValidatorGp validator) : base(validator) {
+    }
+
+    public override void OnCompCardLoaded(Component component) {
+        _value = component.Gp5;
     }
 }
