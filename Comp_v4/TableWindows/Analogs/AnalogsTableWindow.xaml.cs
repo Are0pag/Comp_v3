a380c2cedd41c6513745e0696c2dbcaaa641635a
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-16 15:39:03 +0300

    Analogs UI

---
+++ Comp_v4/TableWindows/Analogs/AnalogsTableWindow.xaml.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+using System.Windows.Input;
+using Comp_v4.TableWindows.Analogs.Buttons;
+using Comp_v4.TableWindows.Analogs.Events;
+using Utils.EventBus;
+
+namespace Comp_v4.TableWindows.Analogs;
+
+public partial class AnalogsTableWindow : Window, IDisposable
+{
+    public AnalogsTableWindow(AnalogsTableVm analogsTableVm, AddAnalogButtonVm addAnalogButtonVm) {
+        InitializeComponent();
+        MainDataGrid.DataContext = analogsTableVm;
+        AddAnalogButton.DataContext = addAnalogButtonVm;
+    }
+
+    public void Dispose() {
+        
+    }
+
+    private void MainDataGrid_OnMouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        EventBus<IAnalogsTableWindowSubscriber>.RaiseEvent<IMouseDoubleClickHandler>(h => h.OnMouseDoubleClick(sender, e));
+    }
+}
---
+++ Comp_v4/TableWindows/Analogs/FormWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace Comp_v4.TableWindows.Analogs;
+
+public partial class FormWindow : Window, IDisposable
+{
+    public FormWindow() {
+        InitializeComponent();
+    }
+
+    public void Dispose() {
+        
+    }
+}
