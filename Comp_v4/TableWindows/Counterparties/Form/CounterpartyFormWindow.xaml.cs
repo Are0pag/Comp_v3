7f4ec286289143b5db5a13290d25670d53cab588
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-21 14:26:33 +0300

    Counterparty Form

---
+++ Comp_v4/TableWindows/Counterparties/Form/CounterpartyFormWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace Comp_v4.TableWindows.Counterparties;
+
+public partial class CounterpartyFormWindow : Window
+{
+    public CounterpartyFormWindow() {
+        InitializeComponent();
+    }
+}
