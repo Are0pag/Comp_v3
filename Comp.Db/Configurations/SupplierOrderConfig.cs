eb2df0f719b5e54252bed97d56d4803cb07d7d14
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-22 11:54:43 +0300

    SupplierOrder config

---
+++ Comp.Db/Configurations/SupplierOrderConfig.cs
@@ -0,0 +1,58 @@
+using Comp.ModelData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Comp.Db.Configurations;
+
+public class SupplierOrderConfig : IEntityTypeConfiguration<SupplierOrder>
+{
+    public void Configure(EntityTypeBuilder<SupplierOrder> builder) {
+        builder.ToTable("SupplierOrders")
+               .HasKey(p => p.Id);
+
+        builder.Property(p => p.Id)
+               .ValueGeneratedOnAdd();
+
+        
+        builder.Property(p => p.PurchaseOrderNumber)
+               .IsRequired();
+        
+        builder.Property(p => p.InvoiceNumber)
+               .IsRequired(false);
+        
+        builder.Property(p => p.Note)
+               .IsRequired(false);
+        
+
+        builder.Property(p => p.OrderStatus)
+               .IsRequired()
+               .HasDefaultValue(OrderStatus.Created.ToString());
+        
+        builder.Property(p => p.VatStatus)
+               .IsRequired()
+               .HasDefaultValue(VatStatus.VatIncluded.ToString());
+        
+        
+        builder.Property(p => p.ContractFilePath)
+               .IsRequired(false);        
+        
+        builder.Property(p => p.InvoiceFilePath)
+               .IsRequired(false);
+        
+        
+        builder.Property(p => p.OrderDate)
+               .IsRequired()
+               .HasDefaultValue(DateTime.Now);
+        
+        builder.Property(p => p.DeliveryDate)
+               .IsRequired()
+               .HasDefaultValue(DateTime.Now);
+        
+        
+        builder.HasOne(p => p.Counterparty)
+               .WithMany()
+               .HasForeignKey(p => p.CounterpartyId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
