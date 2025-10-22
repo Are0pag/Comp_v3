635c1c9c839d9c6c0c372c064fe69eae25dfe6dc
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-22 14:17:28 +0300

    So Table UI xaml

---
+++ Comp_v4/TableWindows/SupplierOrders/Table/SupplierOrderTableWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace Comp_v4.TableWindows.SupplierOrders.Table;
+
+public partial class SupplierOrderTableWindow : Window
+{
+    public SupplierOrderTableWindow() {
+        InitializeComponent();
+    }
+}
