7e2181bbecc76eae01174a5af7be4f58fca855f3
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-23 12:51:40 +0300

    Initial Migration

---
+++ Comp.Db/Migrations/20251023094440_InitialCreate.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comp.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsExpanded = table.Column<bool>(type: "BIT", nullable: false),
+                    ParentCategoryId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Categories_ParentCategoryId",
+                        column: x => x.ParentCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConditionalDesignations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 250, nullable: false),
+                    Designation = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConditionalDesignations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Counterparties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CounterpartyTypeName = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: true),
+                    CityName = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Tin = table.Column<string>(type: "TEXT", nullable: true),
+                    ReasonCode = table.Column<string>(type: "TEXT", nullable: true),
+                    BankName = table.Column<string>(type: "TEXT", nullable: true),
+                    SettlementAccount = table.Column<string>(type: "TEXT", nullable: true),
+                    MinimumOrderAmount = table.Column<string>(type: "TEXT", nullable: true),
+                    IsVatTaxpayer = table.Column<bool>(type: "BIT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Website = table.Column<string>(type: "TEXT", nullable: true),
+                    WebsiteLogin = table.Column<string>(type: "TEXT", nullable: true),
+                    WebsitePassword = table.Column<string>(type: "TEXT", nullable: true),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counterparties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenericParametersSets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    GpMain = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp1 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp2 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp3 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp4 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp5 = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenericParametersSets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Designation = table.Column<string>(type: "TEXT", nullable: false),
+                    FullName = table.Column<string>(type: "TEXT", nullable: true),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    Remark = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeasurementUnits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Designation = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementUnits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeSizes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Designation = table.Column<string>(type: "TEXT", nullable: false),
+                    OutputsNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsUsingSmd = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeSizes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SupplierOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PurchaseOrderNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    InvoiceNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Note = table.Column<string>(type: "TEXT", nullable: true),
+                    OrderStatus = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "Created"),
+                    VatStatus = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "VatIncluded"),
+                    ContractFilePath = table.Column<string>(type: "TEXT", nullable: true),
+                    InvoiceFilePath = table.Column<string>(type: "TEXT", nullable: true),
+                    OrderDate = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()"),
+                    DeliveryDate = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()"),
+                    CounterpartyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupplierOrders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SupplierOrders_Counterparties_CounterpartyId",
+                        column: x => x.CounterpartyId,
+                        principalTable: "Counterparties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GenericParametersSetId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ConditionalDesignationId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ManufacturerId = table.Column<int>(type: "INTEGER", nullable: true),
+                    MeasurementUnitId = table.Column<int>(type: "INTEGER", nullable: true),
+                    TypeSizeId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    NomenclatureNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    CatalogNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    LabelingOptions = table.Column<string>(type: "TEXT", nullable: false),
+                    CodeOfElement = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    UrlAlternative = table.Column<string>(type: "TEXT", nullable: false),
+                    FilePath = table.Column<string>(type: "TEXT", nullable: false),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: true),
+                    QrCodeData = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Comments = table.Column<string>(type: "TEXT", nullable: false),
+                    GpMain = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp1 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp2 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp3 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp4 = table.Column<string>(type: "TEXT", nullable: false),
+                    Gp5 = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Components_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Components_ConditionalDesignations_ConditionalDesignationId",
+                        column: x => x.ConditionalDesignationId,
+                        principalTable: "ConditionalDesignations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Components_GenericParametersSets_GenericParametersSetId",
+                        column: x => x.GenericParametersSetId,
+                        principalTable: "GenericParametersSets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Components_Manufacturers_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Manufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Components_MeasurementUnits_MeasurementUnitId",
+                        column: x => x.MeasurementUnitId,
+                        principalTable: "MeasurementUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Components_TypeSizes_TypeSizeId",
+                        column: x => x.TypeSizeId,
+                        principalTable: "TypeSizes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Analogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SourceComponentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RelatedComponentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsAllCount = table.Column<bool>(type: "BIT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Analogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Analogs_Components_RelatedComponentId",
+                        column: x => x.RelatedComponentId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Analogs_Components_SourceComponentId",
+                        column: x => x.SourceComponentId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Analogs_RelatedComponentId",
+                table: "Analogs",
+                column: "RelatedComponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Analogs_SourceComponentId",
+                table: "Analogs",
+                column: "SourceComponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_CategoryId",
+                table: "Components",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_ConditionalDesignationId",
+                table: "Components",
+                column: "ConditionalDesignationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_GenericParametersSetId",
+                table: "Components",
+                column: "GenericParametersSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_ManufacturerId",
+                table: "Components",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_MeasurementUnitId",
+                table: "Components",
+                column: "MeasurementUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_Name",
+                table: "Components",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_NomenclatureNumber",
+                table: "Components",
+                column: "NomenclatureNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_TypeSizeId",
+                table: "Components",
+                column: "TypeSizeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenericParametersSets_Name",
+                table: "GenericParametersSets",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupplierOrders_CounterpartyId",
+                table: "SupplierOrders",
+                column: "CounterpartyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Analogs");
+
+            migrationBuilder.DropTable(
+                name: "SupplierOrders");
+
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "Counterparties");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "ConditionalDesignations");
+
+            migrationBuilder.DropTable(
+                name: "GenericParametersSets");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+
+            migrationBuilder.DropTable(
+                name: "MeasurementUnits");
+
+            migrationBuilder.DropTable(
+                name: "TypeSizes");
+        }
+    }
+}
