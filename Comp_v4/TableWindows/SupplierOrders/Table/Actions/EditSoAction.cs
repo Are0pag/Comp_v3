c11801b5e8633ef04410c3eb1f50e0397d85f775
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-10-28 14:51:49 +0300

    edit so base

--- Comp_v4/TableWindows/SupplierOrders/Table/Actions/EditSoAction.cs
+++ Comp_v4/TableWindows/SupplierOrders/Table/Actions/EditSoAction.cs
@@ -1,17 +1,19 @@
+using Comp_v4.TableWindows.SupplierOrders.Form;
+using Microsoft.Extensions.DependencyInjection;
 using Utils.WPF.Buttons;
 
 namespace Comp_v4.TableWindows.SupplierOrders.Table.Actions;
 
 public class EditSoAction : BaseActionAsyncSelfWaiting
 {
-    public EditSoAction(BaseButtonAdvanced button) : base(button) {
+    protected readonly IServiceScopeFactory _scopeFactory;
+    public EditSoAction(BaseButtonAdvanced button, IServiceScopeFactory scopeFactory) : base(button) {
+        _scopeFactory = scopeFactory;
     }
 
     public override async Task Perform(TaskCompletionSource tcs) {
-        /*_currentTcs = tcs;
+        _currentTcs = tcs;
         using (var scope = _scopeFactory.CreateScope()) {
-            //var window = scope.ServiceProvider.GetRequiredService<ISupplierOrderFormWindowFactory>().Create(new SupplierOrder());
-            // и тут надо будет перезаписать но как получить ссылку на IServiceCollection?
             var window = scope.ServiceProvider.GetRequiredService<SupplierOrderFormWindow>();
             window.Closed += (sender, args) => {
                 _currentTcs.TrySetResult();
@@ -19,6 +21,6 @@ public class EditSoAction : BaseActionAsyncSelfWaiting
             window.Show();
         
             await _currentTcs.Task;
-        }*/
+        }
     }
 }
