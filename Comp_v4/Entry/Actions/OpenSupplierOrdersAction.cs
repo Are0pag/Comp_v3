7f6859cd49c8aead366d89ca2ce3f0e4047f8f3d
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-11-05 14:05:12 +0300

    разделение метода ожидание результата окна и времени жизни скоупа

--- Comp_v4/Entry/Actions/OpenSupplierOrdersAction.cs
+++ Comp_v4/Entry/Actions/OpenSupplierOrdersAction.cs
@@ -2,30 +2,28 @@ using Comp_v4.Entry.Vm.Buts;
 using Comp_v4.TableWindows.SupplierOrders.Table;
 using Comp_v4.TableWindows.SupplierOrders.Table.Actions;
 using Microsoft.Extensions.DependencyInjection;
+using Templates.Common.Actions;
 using Utils.WPF.Buttons;
 
 namespace Comp_v4.Entry.Actions;
 
-public class OpenSupplierOrdersAction : BaseActionAsyncCompletion
+public class OpenSupplierOrdersAction : BaseActionAsyncScopeHandler
 {
-    protected readonly IServiceScopeFactory _scopeFactory;
-    protected TaskCompletionSource? _currentTcs;
-    public OpenSupplierOrdersAction(OrdersButVm button, IServiceScopeFactory scopeFactory) : base(button) {
-        _scopeFactory = scopeFactory;
+    public OpenSupplierOrdersAction(OrdersButVm button, IServiceScopeFactory scopeFactory) 
+        : base(button, scopeFactory) {
     }
 
     public override async Task Perform(TaskCompletionSource tcs) {
         _currentTcs = tcs;
-        using (var scope = _scopeFactory.CreateScope()) {
-            var window = scope.ServiceProvider.GetRequiredService<SupplierOrderTableWindow>();
-            scope.ServiceProvider.GetRequiredService<AddSoAction>().ParentScope = scope;
-            scope.ServiceProvider.GetRequiredService<EditSoAction>().ParentScope = scope;
-            window.Closed += (_, __) => {
-                _currentTcs.TrySetResult();
-            };
-            window.Show();
-            await tcs.Task;
-        }
+
+        _currentScope = _scopeFactory.CreateScope();
+        
+        var window = _currentScope.ServiceProvider.GetRequiredService<SupplierOrderTableWindow>();
+        _currentScope.ServiceProvider.GetRequiredService<AddSoAction>();
+        _currentScope.ServiceProvider.GetRequiredService<EditSoAction>();
+        window.Closed += OnWindowClosed;
+        window.Show();
+        await tcs.Task;
     }
 
     public override bool CanPerform() {
--- Templates.Common/Actions/BaseActionAsyncScopeHandler.cs
+++ Templates.Common/Actions/BaseActionAsyncScopeHandler.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Utils.WPF.Buttons;
 
@@ -6,9 +7,25 @@ namespace Templates.Common.Actions;
 public abstract class BaseActionAsyncScopeHandler : BaseActionAsyncSelfWaiting
 {
     protected readonly IServiceScopeFactory _scopeFactory;
+    protected IServiceScope? _currentScope;
+    
     public BaseActionAsyncScopeHandler(BaseButtonAdvanced button, IServiceScopeFactory scopeFactory) : base(button) {
         _scopeFactory = scopeFactory;
     }
     
     public IServiceScope? ParentScope { get; set; }
+    
+    protected void OnWindowClosed(object? sender, EventArgs e) {
+        if (sender is Window window) {
+            window.Closed -= OnWindowClosed;
+        }
+        Cleanup();
+    }
+
+    protected void Cleanup() {
+        _currentTcs?.TrySetResult();
+        _currentScope?.Dispose();
+        _currentScope = null;
+        _currentTcs = null;
+    }
 }
