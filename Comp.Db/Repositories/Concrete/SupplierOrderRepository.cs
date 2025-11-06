6c989b51c7fab15b3254dcf659d81c229ecdb10b
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-11-06 16:39:27 +0300

    и AddAsync забыл

--- Comp.Db/Repositories/Concrete/SupplierOrderRepository.cs
+++ Comp.Db/Repositories/Concrete/SupplierOrderRepository.cs
@@ -19,12 +19,16 @@ public class SupplierOrderRepository : DbRepository<SupplierOrder>
         Validate(entity);
 
         entity.Id = default;
-        _context.Counterparties.Attach(entity.Counterparty);
+        entity.Counterparty = await _context.Set<Counterparty>()
+                                            .FirstOrDefaultAsync(o => o.Id == entity.Counterparty.Id)
+                              ?? throw new KeyNotFoundException();
         await base.AddAsync(entity);
     }
 
     public override async Task UpdateAsync(SupplierOrder entity) {
         Validate(entity);
+        entity.Counterparty.Id.ThrowIfDefault();
+        
         var trackedEntity = await _context.Set<SupplierOrder>()
                                           //.Include(o => o.Counterparty)
                                           .FirstOrDefaultAsync(o => o.Id == entity.Id)
@@ -34,12 +38,10 @@ public class SupplierOrderRepository : DbRepository<SupplierOrder>
         entity.CopyTo(trackedEntity);
         if (trackedEntity.CounterpartyId != entity.CounterpartyId) {
             
-            var trackedCounterparty = await _context.Set<Counterparty>()
-                                                .FirstOrDefaultAsync(o => o.Id == entity.CounterpartyId)
-                                  ?? throw new KeyNotFoundException();
-            
-            trackedEntity.Counterparty = trackedCounterparty;
             trackedEntity.CounterpartyId = entity.CounterpartyId;
+            trackedEntity.Counterparty = await _context.Set<Counterparty>()
+                                                       .FirstOrDefaultAsync(o => o.Id == entity.CounterpartyId)
+                                         ?? throw new KeyNotFoundException();
         }
             
         _context.Entry(trackedEntity).State = EntityState.Modified;
@@ -49,6 +51,5 @@ public class SupplierOrderRepository : DbRepository<SupplierOrder>
     protected void Validate(SupplierOrder entity) {
         ArgumentNullException.ThrowIfNull(entity.PurchaseOrderNumber);
         ArgumentNullException.ThrowIfNull(entity.Counterparty);
-        entity.Counterparty.Id.ThrowIfDefault();
     }
 }
