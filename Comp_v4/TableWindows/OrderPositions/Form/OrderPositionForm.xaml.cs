69b392eb0d2e5e8d3ede103246b7a54c8545a4e8
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-12-04 12:54:07 +0300

    простые чил поля

--- Comp_v4/TableWindows/OrderPositions/Form/OrderPositionForm.xaml.cs
+++ Comp_v4/TableWindows/OrderPositions/Form/OrderPositionForm.xaml.cs
@@ -1,10 +1,31 @@
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Input;
+using Comp.ModelData;
 
 namespace Comp_v4.TableWindows.OrderPositions.Form;
 
 public partial class OrderPositionForm : Window
 {
-    public OrderPositionForm() {
+    protected readonly OrderPosition _orderPosition;
+    public OrderPositionForm(OrderPosition orderPosition) {
         InitializeComponent();
+        _orderPosition = orderPosition;
+        DataContext = _orderPosition;
     }
+
+#region Невозможность прописать буквы и т.д. в поле
+
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) {
+        // Разрешаем только цифры
+        e.Handled = !IsTextAllowed(e.Text);
+    }
+
+    private static bool IsTextAllowed(string text) {
+        // Регулярное выражение для проверки, что вводимый текст состоит только из цифр
+        Regex regex = new Regex("[^0-9]+");
+        return !regex.IsMatch(text);
+    }
+
+#endregion
 }
