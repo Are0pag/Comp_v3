643271fb8f2eac51fc6e91f38cdf037acee2f591
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-12-15 10:42:40 +0300

    SupplierOrdersEnumDescriptors3

---
+++ Comp.Db/Migrations/20251215074133_SupplierOrdersDecriptors3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comp.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class SupplierOrdersDecriptors3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ReceiveStatusEnumValue",
+                table: "SupplierOrders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReceiveStatusEnumValue",
+                table: "SupplierOrders");
+        }
+    }
+}
