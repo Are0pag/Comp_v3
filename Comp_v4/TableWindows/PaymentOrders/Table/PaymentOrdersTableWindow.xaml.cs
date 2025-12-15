90c89b8b8014d622f0572769b3a52372e68eed34
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2025-12-15 13:13:58 +0300

    PaymOrd Table Window

--- Comp_v4/TableWindows/PaymentOrders/Table/PaymentOrdersTableWindow.xaml.cs
+++ Comp_v4/TableWindows/PaymentOrders/Table/PaymentOrdersTableWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace Comp_v4.TableWindows.PaymentOrders.Table;
 
@@ -6,5 +7,17 @@ public partial class PaymentOrdersTableWindow : Window
 {
     public PaymentOrdersTableWindow() {
         InitializeComponent();
+    }
+
+    private void DataGrid_OnMouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        throw new NotImplementedException();
+    }
+
+    private void Window_OnPreviewMouseDown(object sender, MouseButtonEventArgs e) {
+        
+    }
+
+    private void Window_OnPreviewKeyDown(object sender, KeyEventArgs e) {
+        
     }
 }
