443d42cc0b2db81d308801de77a44e8d2995e684
Areopag <Mopo3Hoe7yTpo@yandex.ru>
2026-06-22 11:07:13 +0300

    columns

--- Utils/WPF/Windows/Columns/ColumnHeaders.cs
+++ Utils/WPF/Windows/Columns/ColumnHeaders.cs
@@ -2,9 +2,23 @@ namespace Utils.WPF.Windows;
 
 public static class ColumnHeaders
 {
-    public const string Name = "Наименование";
-    public const string NomenclatureNumber = "Номенклатурный номер";
-    public const string CatalogNumber = "Каталожный номер";
-    public const string LabelingOptions = "Варианты маркировки";
-    public const string CodeOfElement = "Код элемента";
+    public const string NAME = "Наименование";
+    public const string NOMENCLATURE_NUMBER = "Номенклатурный номер";
+    public const string CATALOG_NUMBER = "Каталожный номер";
+    public const string LABELING_OPTIONS = "Варианты маркировки";
+    public const string CODE_OF_ELEMENT = "Код элемента";
+    public const string CONDITIONAL_DESIGNATION = "Условное обозначение";
+    public const string MANUFACTURER = "Производитель";
+    public const string MEASUREMENT_UNIT = "Ед. измерения";
+    public const string TYPE_SIZE = "Типоразмер";
+    public const string QR_CODE_DATA = "QR код";
+    public const string DESCRIPTION = "Описание";
+    public const string COMMENTS = "Комментарии";
+    public const string IMAGE_PATH = "Фото";
+    public const string GP_MAIN = "Основной параметр";
+    public const string GP1 = "1-й доп. параметр";
+    public const string GP2 = "2-й доп. параметр";
+    public const string GP3 = "3-й доп. параметр";
+    public const string GP4 = "4-й доп. параметр";
+    public const string GP5 = "5-й доп. параметр";
 }
